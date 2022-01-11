57feec7279b25cf9dcca2840f1a80462bc2cfb1f
Alex <alex.silva@algaworks.com.br>
2022-01-11 12:11:48 -0300

    Atualização aula 23.46 com base no conteúdo de apoio

---
+++ 23.46-externalizando-o-keystore-criando-um-protocolresolver-para-base64-update/algafood-api/src/main/java/com/algaworks/algafood/api/v1/assembler/RestauranteBasicoModelAssembler.java
@@ -0,0 +1,62 @@
+package com.algaworks.algafood.api.v1.assembler;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
+import org.springframework.stereotype.Component;
+
+import com.algaworks.algafood.api.v1.AlgaLinks;
+import com.algaworks.algafood.api.v1.controller.RestauranteController;
+import com.algaworks.algafood.api.v1.model.RestauranteBasicoModel;
+import com.algaworks.algafood.core.security.AlgaSecurity;
+import com.algaworks.algafood.domain.model.Restaurante;
+
+@Component
+public class RestauranteBasicoModelAssembler 
+		extends RepresentationModelAssemblerSupport<Restaurante, RestauranteBasicoModel> {
+
+	@Autowired
+	private ModelMapper modelMapper;
+	
+	@Autowired
+	private AlgaLinks algaLinks;
+	
+	@Autowired
+	private AlgaSecurity algaSecurity;
+	
+	public RestauranteBasicoModelAssembler() {
+		super(RestauranteController.class, RestauranteBasicoModel.class);
+	}
+	
+	@Override
+	public RestauranteBasicoModel toModel(Restaurante restaurante) {
+		RestauranteBasicoModel restauranteModel = createModelWithId(
+				restaurante.getId(), restaurante);
+		
+		modelMapper.map(restaurante, restauranteModel);
+		
+		if (algaSecurity.podeConsultarRestaurantes()) {
+			restauranteModel.add(algaLinks.linkToRestaurantes("restaurantes"));
+		}
+		
+		if (algaSecurity.podeConsultarCozinhas()) {
+			restauranteModel.getCozinha().add(
+					algaLinks.linkToCozinha(restaurante.getCozinha().getId()));
+		}
+		
+		return restauranteModel;
+	}
+	
+	@Override
+	public CollectionModel<RestauranteBasicoModel> toCollectionModel(Iterable<? extends Restaurante> entities) {
+		CollectionModel<RestauranteBasicoModel> collectionModel = super.toCollectionModel(entities);
+		
+		if (algaSecurity.podeConsultarRestaurantes()) {
+			collectionModel.add(algaLinks.linkToRestaurantes());
+		}
+				
+		return collectionModel;
+	}
+	
+}
