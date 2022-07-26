d09e357f7d4ee951ab78e73b345622f326dbad5a
Alex Silva <alex.silva@algaworks.com>
2022-07-26 17:37:59 -0300

    26.36

--- 26.36-corrigindo-documentacao-ocultado-root-entry-point/src/main/java/com/algaworks/algafood/api/v1/model/FotoProdutoModel.java
+++ 26.36-corrigindo-documentacao-ocultado-root-entry-point/src/main/java/com/algaworks/algafood/api/v1/model/FotoProdutoModel.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.api.v1.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.hateoas.RepresentationModel;
@@ -9,9 +10,16 @@ import org.springframework.hateoas.server.core.Relation;
 @Setter
 @Getter
 public class FotoProdutoModel extends RepresentationModel<FotoProdutoModel> {
+	@Schema(example = "b8bbd21a-4dd3-4954-835c-3493af2ba6a0_Prime-Rib.jpg")
 	private String nomeArquivo;
+
+	@Schema(example = "Prime Rib ao ponto")
 	private String descricao;
+
+	@Schema(example = "image/jpeg")
 	private String contentType;
+
+	@Schema(example = "202912")
 	private Long tamanho;
-	
+
 }
